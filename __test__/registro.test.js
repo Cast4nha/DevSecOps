7d37310e4aab50a1d734f81dcbff6d38d425cb4d
Felipe Castanheira <felipevcastanheira@gmail.com>
2023-03-15 23:13:14 -0300

    Problema JEST

--- __test__/registro.test.js
+++ __test__/registro.test.js
@@ -2,19 +2,15 @@ const request = require('supertest');
 const { app } = require('../app');
 
 describe('Teste de registro de usuário', () => {
-  test('Deve retornar status 422 ao criar um usuário existente', (done) => {
-    request(app)
+  test('Deve retornar status 422 ao criar um usuário existente', async () => {
+    const response = await request(app)
       .post('/auth/registro')
       .send({
         nome: 'Fulano',
         email: 'fulano@teste.com',
         senha: 'senha123',
         confirmasenha: 'senha123',
-      })
-      .then((response) => {
-        expect(response.statusCode).toBe(422);
-        done();
-      })
-      .catch((err) => done(err));
-  });
+      });
+    expect(response.statusCode).toBe(422);
+  }, 10000); // timeout de 10 segundos
 });
