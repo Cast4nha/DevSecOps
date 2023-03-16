ce418ccdbfa2caee5314ea1a362a0d229bee0bdf
Felipe Castanheira <felipevcastanheira@gmail.com>
2023-03-15 22:23:04 -0300

    Arrumando workflow

--- __test__/registro.test.js
+++ __test__/registro.test.js
@@ -1,16 +1,20 @@
-const request = require('supertest')
-const { app } = require('../app')
+const request = require('supertest');
+const { app } = require('../app');
 
 describe('Teste de registro de usuário', () => {
-    test('Deve retornar status 422 ao criar um usuário existente', async () => {
-        const response = await request(app)
-            .post('/auth/registro')
-            .send({
-                nome: 'Fulano',
-                email: 'fulano@teste.com',
-                senha: 'senha123',
-                confirmasenha: 'senha123'
-            })
-        expect(response.statusCode).toBe(422)
-    })
-})
+  test('Deve retornar status 422 ao criar um usuário existente', (done) => {
+    request(app)
+      .post('/auth/registro')
+      .send({
+        nome: 'Fulano',
+        email: 'fulano@teste.com',
+        senha: 'senha123',
+        confirmasenha: 'senha123',
+      })
+      .then((response) => {
+        expect(response.statusCode).toBe(422);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
